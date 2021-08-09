363cefc052b0cceb64dbd0f5fd24529207e2a938
Gopi S V <gopisivanappan123@outlook.com>
2021-08-08 23:22:18 -0700

    ListingCard (display all articles)

---
+++ src/Project/Website/code/Controllers/TrnListingController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore;
+using Coforge.Project.Website.Models; //Model
+using Sitecore.Links; //LinkManager
+
+namespace Coforge.Project.Website.Controllers
+{
+    public class TrnListingController : Controller
+    {
+        // GET: TrnListing
+        public ActionResult Index()
+        {
+            //ContextItem - currentContext
+            var contextItem = Context.Item;
+
+
+            var listingCardItems = contextItem.GetChildren()
+                   .Select(x => new ListingCard{
+                       ListingCardTitle = x.Fields["ArticleTitle"].Value,
+                       ListingCardDescription = new HtmlString(x.Fields["ArticleDescription"].Value),   
+                       ListingCardUrl = LinkManager.GetItemUrl(x)
+                   }).ToList();
+
+
+            //return listingCardItems to View
+            return View(listingCardItems);
+        }
+    }
+}
---
+++ src/Project/Website/code/Models/ListingCard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coforge.Project.Website.Models
+{
+    public class ListingCard
+    {
+        public  string ListingCardTitle { get; set; }
+        public HtmlString ListingCardDescription { get; set; }
+
+        public string ListingCardUrl { get; set; }
+
+    }
+}
