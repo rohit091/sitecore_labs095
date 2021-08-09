adbaaa861c636080b53170efae17413f57c17147
Gopi S V <gopisivanappan123@outlook.com>
2021-08-09 00:35:55 -0700

    ListingEntity Template(Inheritance Concept) - Student/Staff/Subject

---
+++ src/Project/Website/code/Controllers/TrnListingEntityController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore;
+using Coforge.Project.Website.Models;
+using Sitecore.Links; 
+
+namespace Coforge.Project.Website.Controllers
+{
+    public class TrnListingEntityController : Controller
+    {
+        // GET: TrnListingEntity
+        public ActionResult Index()
+        {
+            //currentContext
+            var contextItem = Context.Item;
+
+            var EntityCardItems = contextItem.GetChildren()
+                .Select(x => new EntityCard
+                {
+                    EntityName = x.Fields["EntityName"].Value,
+                    EntityBrief = new HtmlString(x.Fields["EntityBrief"].Value),
+                    EntityUrl = LinkManager.GetItemUrl(x)
+                }).ToList();
+
+            //return EntityCardItems to view
+            return View(EntityCardItems);
+        }
+    }
+}
---
+++ src/Project/Website/code/Models/EntityCard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coforge.Project.Website.Models
+{
+    public class EntityCard
+    {
+        public  string EntityName { get; set; }
+        public HtmlString EntityBrief { get; set; }
+
+        public string EntityUrl { get; set; }
+    }
+}
