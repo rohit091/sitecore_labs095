edfb334cb734f3491a77fad7f790f9aef209d340
Gopi S V <gopisivanappan123@outlook.com>
2021-08-09 03:07:18 -0700

    Displaying single EntityCard(Student) on DepartmentPage

---
+++ src/Project/Website/code/Controllers/TrnHallOfFameController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Coforge.Project.Website.Models;
+using Sitecore;
+using Sitecore.Links;
+using Sitecore.Mvc.Presentation;
+
+
+
+namespace Coforge.Project.Website.Controllers
+{
+    public class TrnHallOfFameController : Controller
+    {
+        // GET: TrnHallOfFame
+        public ActionResult Index()
+        {
+            //var contextItem = Context.Item;
+            var contextItem = RenderingContext.Current.Rendering.Item;
+
+            var entityCard = new EntityCard
+            {
+                EntityName = contextItem.Fields["EntityName"].Value,
+                EntityBrief = new HtmlString(contextItem.Fields["EntityBrief"].Value),
+                EntityUrl = LinkManager.GetItemUrl(contextItem)
+            };
+
+
+            return View(entityCard);
+        }
+    }
+}
+
