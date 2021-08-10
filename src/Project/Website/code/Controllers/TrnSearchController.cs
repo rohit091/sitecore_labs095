6de634cbd2b5d1f939e9b214c1af9cba87c59c1b
Gopi S V <gopisivanappan123@outlook.com>
2021-08-10 04:31:27 -0700

    SearchBox Form (mvc structure created)

---
+++ src/Project/Website/code/Controllers/TrnSearchController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore;
+using Coforge.Project.Website.Models;
+
+
+namespace Coforge.Project.Website.Controllers
+{
+    public class TrnSearchController : Controller
+    {
+        //=====
+        // GET: TrnSearch
+        //=====
+        [HttpGet]
+        public ActionResult Index()
+        { 
+            //output to get SearchForm
+            return View();
+        }
+
+
+        //=====
+        //POST : TrnSearch
+        //=====
+        //we need to pass SearchTerm Model & Object for new View here
+        [HttpPost]
+        public ActionResult Index(SearchTerm sterm)
+        {
+            //CODE
+
+            //Provide output after POST request
+            //provide full path after the root
+            return View("/Views/TrnSearch/SResult.cshtml");
+        }
+    }
+}
